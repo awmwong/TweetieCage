0d8e8d47c2be83d01281366a15b402d23946e910
awmwong <contact@a-wong.net>
2011-09-13 15:45:27 -0400

    Initial Commit

---
+++ TweetieCage/TweetieCageAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TweetieCageAppDelegate.h
+//  TweetieCage
+//
+//  Created by Anthony Wong on 11-09-13.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetieCageAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
