7d387efa1a1ad01aca12f92583b9771690a8465d
Anthony Wong <contact@a-wong.net>
2011-09-15 14:34:26 -0400

    TCTableViewController implementation

---
+++ TweetieCage/TCTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  TableViewController.h
+//  TweetieCage
+//
+//  Created by Anthony Wong on 11-09-14.
+//  Copyright 2011 Cleverest Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCTableViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+{
+}
+@end
--- TweetieCage/TweetieCageAppDelegate.h
+++ TweetieCage/TweetieCageAppDelegate.h
@@ -7,16 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TCTableViewController.h"
 
 @interface TweetieCageAppDelegate : NSObject <UIApplicationDelegate>
+{
+    TCTableViewController *tableViewController;
+}
 
 @property (nonatomic, retain) UIWindow *window;
-
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
+@property (nonatomic, retain) TCTableViewController *tableViewController;
 
 @end
