c2e2385bfc17d2f5067fe00860b8af303381571f
Anthony Wong <contact@a-wong.net>
2011-09-16 10:01:58 -0400

    Setup MagicalRecord

--- TweetieCage/TweetieCageAppDelegate.m
+++ TweetieCage/TweetieCageAppDelegate.m
@@ -7,9 +7,11 @@
 //
 
 #import "TweetieCageAppDelegate.h"
+#import <YAJLiOS/YAJL.h>
 
 @interface TweetieCageAppDelegate (private)
     // forward declarations for helpers
+-(NSArray*)getDataFromTwitter;
 @end
 
 @implementation TweetieCageAppDelegate
@@ -18,15 +20,27 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
     // Override point for customization after application launch.
     window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     tableViewController = [[[TCTableViewController alloc] initWithStyle:UITableViewStylePlain] autorelease];
-    
+    [self getDataFromTwitter];
     [window setRootViewController:tableViewController];
     
     [window makeKeyAndVisible];
     return YES;
+}
+
+- (NSArray*) getDataFromTwitter
+{
+    NSURL *url = [NSURL URLWithString:@"http://search.twitter.com/search.json?q=anthowong&rpp=100"];
+    NSData *twitterResponse = [NSData dataWithContentsOfURL:url];
+    //NSData *twitterResponse = [[NSData dataWithContentsOfFile:@"/Users/Anthony/Desktop/example.json"] autorelease];
+    NSArray *arrayFromData = [twitterResponse yajl_JSON];
+    NSDictionary *responseDict = (NSDictionary*)arrayFromData;
+    return arrayFromData;
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
